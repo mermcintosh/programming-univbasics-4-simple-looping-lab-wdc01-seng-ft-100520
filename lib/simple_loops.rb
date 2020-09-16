d685b4789eb3097a2ad0b46321c0d32a5c7ae706
Meredith McIntosh <meredith@mcintosh.io>
2020-09-16 15:01:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,37 @@
-# Write your methods here
+def loop_message_five_times(a message string)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+loop_message_five_times('Hello friend')
+
+
+def loop_message_n_times(message, limit)
+  count = 0
+  while count < limit do
+    puts message
+    count += 1
+  end
+end
+loop_message_n_times('Howdy partner', 5)
+
+
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
+end
+output_array(['apple','orange','banana'])
+
+
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+  count += 1
+  end
+end
+return_string_array(['apple','orange','banana'])
