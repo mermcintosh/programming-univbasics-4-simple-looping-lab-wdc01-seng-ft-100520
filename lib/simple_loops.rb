f6d2468abc23f97f11a2ce33cc99e4b957ab162a
Meredith McIntosh <meredith@mcintosh.io>
2020-09-16 15:40:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,9 @@
+require "pry"
 # def loop_message_five_times(a_message_string)
 #   5.times{puts a_message_string}
 # end
 # loop_message_five_times("Hi everyone!")
-require "pry"
+
 def loop_message_n_times(a_message_string,integar)
   n.times{puts a_message_string}
   n = integar
