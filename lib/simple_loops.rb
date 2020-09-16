34959692f19cd60c2863e71239ff7783599fd37d
Meredith McIntosh <meredith@mcintosh.io>
2020-09-16 15:13:09 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,6 @@
-require "pry"
+
 
 def loop_message_five_times(a message string)
-  a message string = "Hi everyone!"
-  5.times {puts "Hi everyone!"}
+  5.times {puts a message string}
 end
 loop_message_five_times("Hi everyone!")
