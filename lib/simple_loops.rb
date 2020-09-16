80b54c59638f3b3a49fd72d163590bbcd5397d70
Meredith McIntosh <meredith@mcintosh.io>
2020-09-16 16:17:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,30 +3,30 @@ def loop_message_five_times(a_message_string)
 end
 loop_message_five_times("Hi everyone!")
 
-def loop_message_n_times(message, limit)
-  count = 0
-  while count < limit do
-    puts message
-    count += 1
-  end
-end
-loop_messsage_n_times("Hello kiddos",4)
+# def loop_message_n_times(message, limit)
+#   count = 0
+#   while count < limit do
+#     puts message
+#     count += 1
+#   end
+# end
+# loop_messsage_n_times("Hello kiddos",4)
 
-def output_array(array)
-  counter = 0
-  while array[counter] do
-  puts array[counter]
-  counter += 1
-  end
-end
-output_array(['orange', 'red', 'yellow'])
+# def output_array(array)
+#   counter = 0
+#   while array[counter] do
+#   puts array[counter]
+#   counter += 1
+#   end
+# end
+# output_array(['orange', 'red', 'yellow'])
 
-def return_string_array(array)
-  count = 0
-  while count < array.length do
-    array[count] = array[count].to_s
-    count += 1
-  end
-  array
-end
-return_string_array(['Chris', 'Meredith', 'Frank', 'Phyllis'])
+# def return_string_array(array)
+#   count = 0
+#   while count < array.length do
+#     array[count] = array[count].to_s
+#     count += 1
+#   end
+#   array
+# end
+# return_string_array(['Chris', 'Meredith', 'Frank', 'Phyllis'])
