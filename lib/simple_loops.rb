d0bc842537a052316f5f9139f6b0423c79713e5d
Meredith McIntosh <meredith@mcintosh.io>
2020-09-16 16:04:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,11 +3,11 @@ def loop_message_five_times(a_message_string)
 end
 loop_message_five_times("Hi everyone!")
 
-# def loop_message_n_times(a_message_string,integar)
-#   n.times{puts a_message_string}
-#   n = integar
-# end
-# loop_messsage_n_times{"Hello kiddos",4}
+def loop_message_n_times(a_message_string,integar)
+  n.times{puts a_message_string}
+  n = integar
+end
+loop_messsage_n_times{"Hello kiddos",4}
 
 def output_array(array)
   counter = 0
