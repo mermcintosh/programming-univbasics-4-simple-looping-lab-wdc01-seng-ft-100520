680e12ecf415b40ae105d8ccd645bd2aa87c83a8
Meredith McIntosh <meredith@mcintosh.io>
2020-09-16 15:23:09 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,7 @@
-# def loop_message_five_times(a_message_string)
-#   5.times{puts a_message_string}
-# end
-# loop_message_five_times("Hi everyone!")
+def loop_message_five_times(a_message_string)
+  5.times{puts a_message_string}
+end
+loop_message_five_times("Hi everyone!")
 
 def loop_message_n_times(a_message_string,integar)
   puts a_message_string
